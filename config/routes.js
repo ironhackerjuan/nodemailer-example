7652932cb0fe294eb83372fe8d4b3f0b00ba10a3
Juan Romero <jrg89@hotmail.es>
2020-07-16 01:22:23 +0200

    done

--- config/routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const mainController = require('../controllers/main.controller');
-
-router.get('/', mainController.index);
-router.post('/sendform', mainController.handleForm)
-
-
-module.exports = router;
